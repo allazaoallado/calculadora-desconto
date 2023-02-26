70e4b13b7fdd25a2a9a709578c3bf30b9e4c3abf
allazaoallado <allanv985@gmail.com>
2023-02-25 22:37:09 -0300

    ajusando os ifs da pagina para aceitar numero com virgula ex: 2,5

--- script.js
+++ script.js
@@ -1,38 +1,34 @@
 function calculo(){ 
     let desc = window.document.getElementById('desc')
     let preco = window.document.getElementById('preco')
-
-
-    let desconto = Number(desc.value.replace(',','.'))/100 * Number(preco.value.replace(',','.'))
-
-    let valor_final = Number(preco.value.replace(',','.')) - desconto
-
     let valor_desconto = window.document.getElementById('valor_desconto')
     let valor_pagar = window.document.getElementById('valor_pagar')
-
+    
     desc.style.borderColor = "#ffcae1"
     preco.style.borderColor = "#ffcae1"
+   
+
+    preco = parseFloat(preco.value.toString().replace(',','.'))
+    desc = parseFloat(desc.value.toString().replace(',','.'))
 
 
-    valor_desconto.innerHTML = `O valor de desconto é de: R$ ${desconto.toFixed(2).replace('.',',')}`
-    valor_pagar.innerHTML = `O valor total a ser pago é de: R$ ${valor_final.toFixed(2).replace('.',',')}`
-    if(desc.value == 0 && preco.value == 0){
-        desc.style.borderColor = "#FF0F0F"
+
+
+    let desconto = (desc/100) * preco
+    if(preco < 0 || isNaN(preco)){
+        let preco = window.document.getElementById('preco')
         preco.style.borderColor = "#FF0F0F"
-        window.alert('Preencha os campos!')
-        valor_desconto.innerHTML = `O valor de desconto é de:`
-        valor_pagar.innerHTML = `O valor total a ser pago é de:`
-    }else if(preco.value <= 0 || isNaN(preco.value)){
-        window.alert('Insira um preço válido válido')
-        valor_desconto.innerHTML = `O valor de desconto é de:`
-        valor_pagar.innerHTML = `O valor total a ser pago é de:`
-        preco.style.borderColor = "#FF0F0F"
-    }else if(desc.value <= 0 || isNaN(desc.value)){
-        window.alert('Insira um preço válido')
-        valor_desconto.innerHTML = `O valor de desconto é de:`
-        valor_pagar.innerHTML = `O valor total a ser pago é de:`
+        window.alert('Preencha o campo Preço corretamente!')
+    }else if(desc < 0 || isNaN(desc)){
+        let desc = window.document.getElementById('desc')
         desc.style.borderColor = "#FF0F0F"
+        window.alert('Preencha o campo Desc % corretamente!')
+    }else{
+        valor_desconto.innerHTML = `O valor de desconto é de: R$ ${desconto.toFixed(2).replace('.',',')}`
+        valor_pagar.innerHTML = `O valor total a ser pago é de: R$ ${(preco - desconto).toFixed(2).replace('.',',')}`
     }
+
+
 
 }
 
@@ -51,34 +47,38 @@ function calculo2(){
     
     let preco_real = window.document.getElementById('preco_real')
     let preco_final = window.document.getElementById('preco_final')
-
-    let desconto = 100 - ((100 * Number(preco_final.value.replace(',','.')))/ Number(preco_real.value.replace(',','.')))
-
-
-    let resultado_2 = window.document.getElementById('resultado_2')
-    resultado_2.innerHTML =`O desconto a ser aplicado é de: ${desconto}%`
     preco_real.style.borderColor = "#ffcae1"
     preco_final.style.borderColor = "#ffcae1"
 
-    if(preco_final.value == 0 && preco_real.value == 0 ){
-    window.alert('Preencha os campos!')
+    preco_real = parseFloat(preco_real.value.toString().replace(',','.'))
+    preco_final = parseFloat(preco_final.value.toString().replace(',','.'))
+    
+
+    let desconto = 100 - (100 * preco_final)/ preco_real
+
+
+    let resultado_2 = window.document.getElementById('resultado_2')
+    
+
+    if( preco_real < 0 || isNaN(preco_real)){
+    window.alert('Preencha campo corretamente!')
+    let preco_real = window.document.getElementById('preco_real')
     resultado_2.innerHTML =`O desconto a ser aplicado é de:`
     preco_real.style.borderColor = "#FF0F0F"
-    preco_final.style.borderColor = "#FF0F0F"
-    }else if(isNaN(preco_real.value) || preco_real.value < 0){
-    window.alert('Insira um preço válido')
-    resultado_2.innerHTML =`O desconto a ser aplicado é de:`
-    preco_real.style.borderColor = "#FF0F0F"
-    }else if(isNaN(preco_final.value) || preco_final.value < 0){
-    window.alert('Insira um preço válido')
+    }else if(preco_final < 0 || isNaN(preco_final)){
+    window.alert('Preencha o campo corretamente!')
+    let preco_final = window.document.getElementById('preco_final')
     resultado_2.innerHTML =`O desconto a ser aplicado é de:`
     preco_final.style.borderColor = "#FF0F0F"
-    }else if(isNaN(desconto)){
-        let resultado_2 = window.document.getElementById('resultado_2')
-        resultado_2.innerHTML =`O desconto a ser aplicado é de: 0,0%`
+
+    }else{
+        let desconto = (100 - (100 * preco_final)/ preco_real)
+        
+        resultado_2.innerHTML =`O desconto a ser aplicado é de: ${desconto.toFixed(2).replace('.',',')}%`
+        
+    }
     }
 
-}
 
 function limpar2(){
     let resultado_2 = window.document.getElementById('resultado_2')
